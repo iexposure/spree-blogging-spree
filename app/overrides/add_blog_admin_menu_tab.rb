1def8ffdadac8525ca457d1eb839fa454ddddb27
alex <alesssandro@ukr.net>
2015-02-13 15:20:10 +0200

    change overrides

--- app/overrides/add_blog_admin_menu_tab.rb
+++ app/overrides/add_blog_admin_menu_tab.rb
@@ -1,5 +1,6 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
+Deface::Override.new(:virtual_path => "spree/admin/shared/main_menu",
                      :name => "blog_admin_tabs",
-                     :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
+                     :insert_bottom => "#main-sidebar",
                      :text => "<%= tab(:blog_entries, :label => 'Blog', :url => spree.admin_blog_entries_path, :icon => 'file') if can? :manage, Spree::BlogEntry %>",
+                     :text => "<% if can? :admin, BlogEntry %><ul class=\"nav nav-sidebar\"><%= tab *[:blog], icon: 'file'  %></ul><% end %>",
                      :disabled => false)
