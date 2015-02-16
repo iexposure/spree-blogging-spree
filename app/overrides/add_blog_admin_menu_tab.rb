4db040d00a5353b63989e507ac147e062902d43a
alex <alesssandro@ukr.net>
2015-02-16 16:17:31 +0200

    modify admin tab and views

--- app/overrides/add_blog_admin_menu_tab.rb
+++ app/overrides/add_blog_admin_menu_tab.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "blog_admin_tabs",
                      :insert_bottom => "#main-sidebar",
-                     :text => "<% if can? :admin, Spree::BlogEntry %><ul class=\"nav nav-sidebar\"><%= tab *[:blog_entries], icon: 'file'  %></ul><% end %>",
+                     :text => "<% if can? :admin, Spree::BlogEntry %><ul class=\"nav nav-sidebar\"><%= tab *[:blog_entries], icon: 'tags'  %></ul><% end %>",
                      :disabled => false)
